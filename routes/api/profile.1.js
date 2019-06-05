062c3f552be2a89c52af252effdf9a9d4ff111b0
Michael Todd <michaelbtodd@yahoo.com>
2019-06-05 16:53:36 -0400

    Get Current user profile_not working

--- routes/api/profile.1.js
+++ routes/api/profile.1.js
@@ -52,15 +52,15 @@ router.post(
 			company,
 			website,
 			location,
-			status,
-			skills,
 			bio,
+			status,
 			githubusername,
+			skills,
 			youtube,
-			twitter,
 			facebook,
-			linkedin,
-			instagram
+			twitter,
+			instagram,
+			linkedin
 		} = req.body;
 
 		// Build profile object
@@ -69,17 +69,17 @@ router.post(
 		if (company) profileFields.company = company;
 		if (website) profileFields.website = website;
 		if (location) profileFields.location = location;
+		if (bio) profileFields.bio = bio;
 		if (status) profileFields.status = status;
+		if (githubusername) profileFields.githubusername = githubusername;
 		if (skills) {
 			profileFields.skills = skills.split(',').map((skill) => skill.trim());
-			if (bio) profileFields.bio = bio;
-			if (githubusername) profileFields.githubusername = githubusername;
 		}
 
 		// Build social object
 		profileFields.social = {};
-		if (youtube) profileFields.social.youtube = youtube;
 		if (twitter) profileFields.social.twitter = twitter;
+		if (youtube) profileFields.social.youtube = youtube;
 		if (facebook) profileFields.social.facebook = facebook;
 		if (linkedin) profileFields.social.linkedin = linkedin;
 		if (instagram) profileFields.social.instagram = instagram;
@@ -91,15 +91,11 @@ router.post(
 				// Update
 				profile = await Profile.findOneAndUpdate(
 					{ user: req.user.id },
-					{
-						$set : profileFields
-					},
+					{ $set: profileFields },
 					{ new: true }
 				);
-
 				return res.json(profile);
 			}
-
 			// Create
 			profile = new Profile(profileFields);
 
