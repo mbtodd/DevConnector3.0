8ac5e1368caf96445f8dbc01b3ab09da41ed8df8
Michael Todd <michaelbtodd@yahoo.com>
2019-06-27 17:03:48 -0400

    Alert Component & Action Call

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navbar from './components/layouts/Navbar';
-import Landing from './components/layouts/Landing';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Alert from './components/layout/Alert';
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
@@ -16,11 +17,12 @@ const App = () => (
 			<Fragment>
 				<Navbar />
 				{/* <Landing /> */}
-				<Route exact path="/" component={Landing} />
-				<section className="container">
+				<Route exact path='/' component={Landing} />
+				<section className='container'>
+					<Alert />
 					<Switch>
-						<Route exact path="/register" component={Register} />
-						<Route exact path="/login" component={Login} />
+						<Route exact path='/register' component={Register} />
+						<Route exact path='/login' component={Login} />
 					</Switch>
 				</section>
 			</Fragment>
