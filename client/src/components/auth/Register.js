70f2223425f11a57088ddae80b817592c3ddd80a
Michael Todd <michaelbtodd@yahoo.com>
2019-06-28 12:36:20 -0400

    auth reducer & register action setup

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -2,11 +2,12 @@ import React, { Fragment, useState } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { setAlert } from '../../actions/alert';
+import { register } from '../../actions/auth';
 import PropTypes from 'prop-types';
 
 // import axios from 'axios';
 
-const Register = ({ setAlert }) => {
+const Register = ({ setAlert, register }) => {
 	const [
 		formData,
 		setFormData
@@ -28,7 +29,7 @@ const Register = ({ setAlert }) => {
 			setAlert('Passwords do not match', 'danger');
 		}
 		else {
-			console.log('SUCCESS');
+			register({ name, email, password });
 			// const newUser = {
 			// 	name,
 			// 	email,
@@ -65,7 +66,7 @@ const Register = ({ setAlert }) => {
 						name='name'
 						value={name}
 						onChange={e => onChange(e)}
-						required
+						// required
 					/>
 				</div>
 				<div className='form-group'>
@@ -75,7 +76,7 @@ const Register = ({ setAlert }) => {
 						name='email'
 						value={email}
 						onChange={e => onChange(e)}
-						required
+						// required
 					/>
 					<small className='form-text'>
 						This site uses Gravatar so if you want a profile image, use a
@@ -89,7 +90,7 @@ const Register = ({ setAlert }) => {
 						name='password'
 						value={password}
 						onChange={e => onChange(e)}
-						minLength='6'
+						// minLength='6'
 					/>
 				</div>
 				<div className='form-group'>
@@ -99,7 +100,7 @@ const Register = ({ setAlert }) => {
 						name='password2'
 						value={password2}
 						onChange={e => onChange(e)}
-						minLength='6'
+						// minLength='6'
 					/>
 				</div>
 				<input type='submit' className='btn btn-primary' value='Register' />
@@ -112,7 +113,8 @@ const Register = ({ setAlert }) => {
 };
 
 Register.propTypes = {
-	setAlert : PropTypes.func.isRequired
+	setAlert : PropTypes.func.isRequired,
+	register : PropTypes.func.isRequired
 };
 
-export default connect(null, { setAlert })(Register);
+export default connect(null, { setAlert, register })(Register);
