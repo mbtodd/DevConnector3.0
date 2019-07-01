e09e50bcd978f579424052951e93db7c2af24db8
Michael Todd <michaelbtodd@yahoo.com>
2019-07-01 15:21:36 -0400

    auth changes on laptop

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,9 +1,34 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import { REGISTER_SUCCESS, REGISTER_FAIL } from './types';
+import {
+	REGISTER_SUCCESS,
+	REGISTER_FAIL,
+	USER_LOADED,
+	AUTH_ERROR
+} from './types';
+import setAuthToken from '../utils/setAuthToken';
+
+// Load User
+export const loadUser = () => async (dispatch) => {
+	if (localStorage.token) {
+		setAuthToken(localStorage.token);
+	}
+	try {
+		const res = await axios.get('/api/auth');
+
+		dispatch({
+			type    : USER_LOADED,
+			payload : res.data
+		});
+	} catch (err) {
+		dispatch({
+			type : AUTH_ERROR
+		});
+	}
+};
 
 // Register User
-export const register = ({ name, email, password }) => async dispatch => {
+export const register = ({ name, email, password }) => async (dispatch) => {
 	const config = {
 		headers : {
 			'Content-Type' : 'application/json'
@@ -23,7 +48,7 @@ export const register = ({ name, email, password }) => async dispatch => {
 		const errors = err.response.data.errors;
 
 		if (errors) {
-			errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+			errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
 		}
 		dispatch({
 			type : REGISTER_FAIL
