1b06e9703f8602bc52695043d9be633f4a039e21
Michael Todd <michaelbtodd@yahoo.com>
2019-07-05 16:47:46 -0400

    user login 1/2 finished.

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -4,12 +4,14 @@ import {
 	REGISTER_SUCCESS,
 	REGISTER_FAIL,
 	USER_LOADED,
-	AUTH_ERROR
+	AUTH_ERROR,
+	LOGIN_SUCCESS,
+	LOGIN_FAIL
 } from './types';
 import setAuthToken from '../utils/setAuthToken';
 
 // Load User
-export const loadUser = () => async (dispatch) => {
+export const loadUser = () => async dispatch => {
 	if (localStorage.token) {
 		setAuthToken(localStorage.token);
 	}
@@ -28,7 +30,7 @@ export const loadUser = () => async (dispatch) => {
 };
 
 // Register User
-export const register = ({ name, email, password }) => async (dispatch) => {
+export const register = ({ name, email, password }) => async dispatch => {
 	const config = {
 		headers : {
 			'Content-Type' : 'application/json'
@@ -44,14 +46,46 @@ export const register = ({ name, email, password }) => async (dispatch) => {
 			type    : REGISTER_SUCCESS,
 			payload : res.data
 		});
+
+		dispatch(loadUser());
 	} catch (err) {
 		const errors = err.response.data.errors;
 
 		if (errors) {
-			errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+			errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
 		}
 		dispatch({
 			type : REGISTER_FAIL
+		});
+	}
+};
+// Login User
+export const login = (email, password) => async dispatch => {
+	const config = {
+		headers : {
+			'Content-Type' : 'application/json'
+		}
+	};
+
+	const body = JSON.stringify({ email, password });
+
+	try {
+		const res = await axios.post('/api/auth', body, config);
+
+		dispatch({
+			type    : LOGIN_SUCCESS,
+			payload : res.data
+		});
+
+		dispatch(loadUser());
+	} catch (err) {
+		const errors = err.response.data.errors;
+
+		if (errors) {
+			errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+		}
+		dispatch({
+			type : LOGIN_FAIL
 		});
 	}
 };
--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,8 +1,11 @@
 import React, { Fragment, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { login } from '../../actions/auth';
 // import axios from 'axios';
 
-const Login = () => {
+const Login = ({ login, isAuthenticated }) => {
 	const [
 		formData,
 		setFormData
@@ -13,49 +16,63 @@ const Login = () => {
 
 	const { email, password } = formData;
 
-	const onChange = (e) =>
+	const onChange = e =>
 		setFormData({ ...formData, [e.target.name]: e.target.value });
 
-	const onSubmit = async (e) => {
+	const onSubmit = async e => {
 		e.preventDefault();
-		console.log('SUCCESS');
+		login(email, password);
 	};
+
+	// Redirect if logged in
+	if (isAuthenticated) {
+		return <Redirect to='/dashboard' />;
+	}
 
 	return (
 		<Fragment>
-			<h1 className="large text-primary">Sign In</h1>
-			<p className="lead">
-				<i className="fas fa-user" /> Sign Into Your Account
+			<h1 className='large text-primary'>Sign In</h1>
+			<p className='lead'>
+				<i className='fas fa-user' /> Sign Into Your Account
 			</p>
-			<form className="form" onSubmit={(e) => onSubmit(e)}>
-				<div className="form-group">
+			<form className='form' onSubmit={e => onSubmit(e)}>
+				<div className='form-group'>
 					<input
-						type="email"
-						placeholder="Email Address"
-						name="email"
+						type='email'
+						placeholder='Email Address'
+						name='email'
 						value={email}
-						onChange={(e) => onChange(e)}
+						onChange={e => onChange(e)}
 						required
 					/>
 				</div>
-				<div className="form-group">
+				<div className='form-group'>
 					<input
-						type="password"
-						placeholder="Password"
-						name="password"
+						type='password'
+						placeholder='Password'
+						name='password'
 						value={password}
-						onChange={(e) => onChange(e)}
-						minLength="6"
+						onChange={e => onChange(e)}
+						minLength='6'
 					/>
 				</div>
 
-				<input type="submit" className="btn btn-primary" value="Login" />
+				<input type='submit' className='btn btn-primary' value='Login' />
 			</form>
-			<p className="my-1">
-				Don't have an account? <Link to="/register">Sign Up</Link>
+			<p className='my-1'>
+				Don't have an account? <Link to='/register'>Sign Up</Link>
 			</p>
 		</Fragment>
 	);
 };
 
-export default Login;
+login.PropTypes = {
+	login           : PropTypes.func.isRequired,
+	isAuthenticated : PropTypes.bool
+};
+
+const mapStateToProps = state => ({
+	isAuthenticated : state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps, { login })(Login);
